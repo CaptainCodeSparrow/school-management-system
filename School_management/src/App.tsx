66a649d7e1cf34a4c4e72f139375b84e573bcec0
captaincodesparrow <captaincodesparrow@gmail.com>
2024-11-26 17:35:47 +0600

    .

---
+++ School_management/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <div className="text-xl text-blue-500 p-5">CaptainCodeSparow</div>
+      </div>
+  )
+}
+
+export default App
